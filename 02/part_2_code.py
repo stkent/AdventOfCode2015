11990f0e1d34f087e576a97f9ad42e979b32728d
Stuart Kent <stuart@detroitlabs.com>
2015-12-04 11:58:45 -0500

    Add day 2 part 2

---
+++ 02/part_2_code.py
@@ -0,0 +1,16 @@
+import shared_code
+
+
+def get_ribbon_length_for_dimensions(dimensions):
+    wrap_ribbon_length = 2*(dimensions[0] + dimensions[1])
+    bow_ribbon_length = reduce(lambda x, y: x*y, dimensions, 1)
+    total_ribbon_length = wrap_ribbon_length + bow_ribbon_length
+
+    return total_ribbon_length
+
+
+parsed_dimensions = shared_code.get_parsed_dimensions()
+
+total_wrapping_paper_area = sum([get_ribbon_length_for_dimensions(dimensions) for dimensions in parsed_dimensions])
+
+print "Total ribbon length: " + str(total_wrapping_paper_area)
