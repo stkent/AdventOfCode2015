0a98ffdac6617979c7a9b7bbf7fadf05f894f051
Stuart Kent <stuart@detroitlabs.com>
2015-12-11 16:29:29 -0500

    Add day 11

---
+++ 11/code.py
@@ -0,0 +1,81 @@
+def is_increasing_sequence(string):
+    if len(string) == 0:
+        return False
+
+    ordinal_list = [ord(character) for character in string]
+    ordinal_deltas = [ordinal_list[i + 1] - ordinal_list[i] for i in range(len(ordinal_list) - 1)]
+
+    return len(filter(lambda d: d != 1, ordinal_deltas)) == 0
+
+
+def contains_increasing_sequence_of_at_least_three_characters(string):
+    for i in range(len(string) - 3):
+        candidate_substring = string[i:i + 3]
+
+        if is_increasing_sequence(candidate_substring):
+            return True
+
+    return False
+
+
+def contains_at_least_two_distinct_pairs(string):
+    number_of_distinct_pairs = 0
+
+    index = 0
+    while index < len(string) - 1:
+        if string[index] == string[index + 1]:
+            number_of_distinct_pairs += 1
+            index += 2
+        else:
+            index += 1
+
+    return number_of_distinct_pairs >= 2
+
+
+def is_valid_password(string):
+    if not contains_increasing_sequence_of_at_least_three_characters(string):
+        return False
+
+    for banned_character in ["i", "o", "l"]:
+        if banned_character in string:
+            return False
+
+    if not contains_at_least_two_distinct_pairs(string):
+        return False
+
+    return True
+
+
+def increment_character(character):
+    return unichr(ord('a') + (ord(character) - ord('a') + 1) % (ord('z') - ord('a') + 1))
+
+
+def increment_string(string):
+    result_tail = []
+
+    index = len(string) - 1
+    while index >= 0:
+        result_tail.insert(0, increment_character(string[index]))
+
+        # If we didn't just wrap from z to a, we are done incrementing this string.
+        if result_tail[0] != 'a':
+            break
+        else:
+            index -= 1
+
+    return string[0:index] + "".join(result_tail)
+
+
+def compute_next_valid_password(string):
+    while not is_valid_password(string):
+        string = increment_string(string)
+
+    return string
+
+
+if __name__ == '__main__':
+    # current_password = "hepxcrrq"
+    current_password = "hepxxzaa"
+
+    print "The next valid password for input " + current_password +\
+          " is " + compute_next_valid_password(current_password)
---
+++ 11/tests.py
@@ -0,0 +1,29 @@
+import unittest
+import code
+
+
+class Part1Tests(unittest.TestCase):
+
+    def test_validity_check_with_first_example_input(self):
+        self.assertFalse(code.is_valid_password("hijklmmn"))
+
+    def test_validity_check_with_second_example_input(self):
+        self.assertFalse(code.is_valid_password("abbceffg"))
+
+    def test_validity_check_with_third_example_input(self):
+        self.assertFalse(code.is_valid_password("abcddgjk"))
+
+    def test_validity_check_with_good_password(self):
+        self.assertTrue(code.is_valid_password("ddabcee"))
+
+    def test_next_password_calculator_with_first_example_input(self):
+        self.assertEqual(code.compute_next_valid_password("abcdefgh"), "abcdffaa")
+
+    def test_next_password_calculator_with_second_example_input(self):
+        self.assertEqual(code.compute_next_valid_password("ghijklmn"), "ghjaabcc")
+
+    def test_incrementing_a(self):
+        self.assertEqual(code.increment_character("a"), "b")
+
+    def test_incrementing_z(self):
+        self.assertEqual(code.increment_character("z"), "a")
