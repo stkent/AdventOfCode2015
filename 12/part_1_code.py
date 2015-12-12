f883b1072c3a2092951a3b15840c59a02642170d
Stuart Kent <stuart@detroitlabs.com>
2015-12-12 11:48:23 -0500

    Add day 12 part 1

--- 12/part_1_code.py
+++ 12/part_1_code.py
@@ -1,2 +1,12 @@
+import re
+
+
 def compute_sum(string):
-    return -1
+    return sum([int(n) for n in re.findall("-?\d+", string)])
+
+
+if __name__ == "__main__":
+    with open("input.txt") as input_file:
+        input_string = input_file.readline()
+
+        print "Total sum of numbers in structure: " + str(compute_sum(input_string))
